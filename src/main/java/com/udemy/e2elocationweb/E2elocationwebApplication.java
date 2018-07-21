49ec39a43fa8430712d055b3e9c9ede3a55a7d68
govital3 <govital3@gmail.com>
2018-07-21 13:51:38 +0300

    location.java: geters and setters and a tostring method.

---
+++ src/main/java/com/udemy/e2elocationweb/E2elocationwebApplication.java
@@ -0,0 +1,12 @@
+package com.udemy.e2elocationweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class E2elocationwebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(E2elocationwebApplication.class, args);
+    }
+}
