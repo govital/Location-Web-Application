b84810ed514a6866af2a12683ade5a1c1ae71dce
govital3 <govital3@gmail.com>
2018-07-27 10:50:19 +0300

    added: completed flow of saveLocation + display $msg on save success. added: displayLocations.jsp added: web context to app.properties.

--- src/main/java/com/udemy/e2elocationweb/controllers/LocationController.java
+++ src/main/java/com/udemy/e2elocationweb/controllers/LocationController.java
@@ -1,15 +1,42 @@
 package com.udemy.e2elocationweb.controllers;
 
+import com.udemy.e2elocationweb.entities.Location;
+import com.udemy.e2elocationweb.service.LocationService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 public class LocationController {
+
+    @Autowired
+    LocationService service;
 
     @RequestMapping("/showCreate")
     public String showCreate() {
         return "createLocation";
     }
+
+    @RequestMapping("saveLoc")
+    public String saveLocation(@ModelAttribute("location")Location location, ModelMap modelMap){
+        Location savedLocation = service.saveLocation(location);
+        String msg = "location saved with id: " + savedLocation.getId();
+        modelMap.addAttribute("msg", msg);
+        return "createLocation";
+    }
+
+    @RequestMapping("displayLocations")
+    public String getAllLocations(@ModelAttribute("location")Location location, ModelMap modelMap){
+        List<Location> locations = service.getAllLocations();
+        modelMap.addAttribute("locations", locations);
+        return "displayLocations";
+    }
+
+
 
     @RequestMapping("/showHello")
     public String showHello() {
